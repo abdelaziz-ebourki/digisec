b13b927aa1399ca17b9f4b96e34e3080ef9f6bb1
abdelaziz-ebourki <abdelaziz-ebourki@users.noreply.github.com>
2026-08-26 19:09:47 +0100

    feat(ui): add Accueil1/Accueil2 hero previews (faithful modern recreation of legacy hero)

---
+++ ui/src/pages/Accueil2.tsx
@@ -0,0 +1,11 @@
+import { HeroLegacy } from '@/components/home/HeroLegacy'
+import { HomeContent } from '@/components/home/HomeContent'
+
+export default function Accueil2() {
+  return (
+    <>
+      <HeroLegacy />
+      <HomeContent />
+    </>
+  )
+}
